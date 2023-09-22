c0012fdca8300ce909d20fe2c81e14c3b4eed5e2
dkoshy <deepakkoshy08@gmail.com>
2023-09-22 14:33:28 +0530

    Basic Example 1

---
+++ EF6 Examples/PublisherConsole/BasicExamples.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherData;
+using PublisherDomain;
+
+namespace PublisherConsole
+{
+    public class BasicExamples
+    {
+
+
+        public void Coordinator()
+        {
+            using (var dbContext = new PublisherDBContext())
+            {
+                dbContext.Database.EnsureCreated();
+            }
+
+            //GetAuthors();
+            //AddAuthor();
+            //GetAuthors();
+            //AddAuthorWithBook();
+            GetAuthorsWithBooks();
+        }
+
+
+
+        void GetAuthors()
+        {
+            var ctx = new PublisherDBContext();
+            var query = ctx.Authors;
+            foreach (var author in query)
+            {
+                Console.WriteLine($"{author.FirstName}, {author.LastName}");
+            }
+        }
+
+
+        void AddAuthor()
+        {
+            var ctx = new PublisherDBContext();
+            ctx.Authors.Add(new Author { FirstName="Deepak", LastName="Koshy" });
+            ctx.SaveChanges();
+        }
+
+        void AddAuthorWithBook()
+        {
+            var ctx = new PublisherDBContext();
+            ctx.Authors.Add(new Author { FirstName="Thomas", LastName="Koshy", Books=new List<Book>() { new Book { Title="History of Thazyayill." } } });
+            ctx.SaveChanges();
+        }
+
+        void GetAuthorsWithBooks()
+        {
+            var ctx = new PublisherDBContext();
+            var authors = ctx.Authors.Include(a => a.Books).ToList();
+
+            foreach (var author in authors)
+            {
+                if (author.Books.Count > 0)
+                    Console.WriteLine($"{author.FirstName}, {author.LastName}");
+                foreach (var book in author.Books)
+                {
+                    Console.WriteLine($"-- {book.Title} --");
+                }
+
+            }
+        }
+
+    }
+}
