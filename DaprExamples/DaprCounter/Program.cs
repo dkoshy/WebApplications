02a3956950c73e7138a4a9a71db4399c1143c9ef
dkoshy <deepakkoshy08@gmail.com>
2023-10-29 23:27:43 +0530

    Dapr Example

---
+++ DaprExamples/DaprCounter/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Dapr.Client;
+
+Console.WriteLine("Hello, - Dapr Counter Applications");
+const string storeName = "statestore";
+const string key = "counter";
+var darpClient = new DaprClientBuilder().Build();
+
+var counter = await darpClient.GetStateAsync<int>(storeName, key);
+
+while (true)
+{
+    Console.WriteLine($"Increment Counter {++counter}");
+    await darpClient.SaveStateAsync(storeName, key,counter);
+    await Task.Delay(1000);
+}
+
