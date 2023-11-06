7e648671037b42e5699278abadb183d32d969fd5
dkoshy <deepakkoshy08@gmail.com>
2023-11-06 11:09:17 +0530

    Rabbit MQ Examples

---
+++ MessagingApps/RabbitMQ.Consumer/Consumer.cs
@@ -0,0 +1,36 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Rabbit Mq consumer.");
+var queeName = "mysamplequeue";
+var rabbitmqFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port=5672
+};
+
+using var conn = rabbitmqFactory.CreateConnection();
+using var channel = conn.CreateModel();
+
+channel.QueueDeclare(queeName, false, false, false, null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    printMessage(message);
+};
+channel.BasicConsume(queeName,true, consumer);
+
+
+
+ void printMessage(string message)
+{
+    Console.WriteLine("{0}", message);
+}
+
+Console.ReadLine();
---
+++ MessagingApps/RbitMQ.Producer/Producer.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("RbbitMq Producer");
+var queeName = "mysamplequeue";
+var rabbitConnFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672
+};
+
+using var conn = rabbitConnFactory.CreateConnection();
+using var channel = conn.CreateModel();
+ channel.QueueDeclare("mysamplequeue", false, false, false, null);
+
+var body = Encoding.UTF8.GetBytes("Getting started with .Net and RabbitMq");
+channel.BasicPublish("", queeName, null, body);
+Console.WriteLine( "Sent messages to ..{0}",queeName );
+
+Console.ReadLine();
