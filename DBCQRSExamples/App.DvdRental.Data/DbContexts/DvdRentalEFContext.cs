503561bfa8a9e4f610fedd94ad4ad99b9b8bf511
Deepak Koshy <deepakkoshy08@gmail.com>
2023-11-17 09:49:27 +0530

    correcting Insert-Category

--- DBCQRSExamples/App.DvdRental.Data/DbContexts/DvdRentalEFContext.cs
+++ DBCQRSExamples/App.DvdRental.Data/DbContexts/DvdRentalEFContext.cs
@@ -10,9 +10,25 @@ namespace App.DvdRental.Data.DbContexts
         public DbSet<Category> Categories { get; set; }
 
         public DvdRentalEFContext(DbContextOptions<DvdRentalEFContext> options)
-        :base(options)
+        : base(options)
         {
-            
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>
+            (c =>{
+                c.Property(p=>p.Category_Id).HasColumnName("category_id");
+                c.Property(p=> p.Name).HasColumnName("name");
+                c.Property(p=>p.Last_Update).HasColumnName("last_update");
+                c.ToTable("category");
+            });
+                
+                 
+                
+                
+                        
         }
     }
 
